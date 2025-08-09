ae03c189862608cc42c3d302ddf7c76c41567898
Saumya Srivastava <saumyasrivastava498@gmail.com>
2025-08-09 21:32:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 
 //routes
