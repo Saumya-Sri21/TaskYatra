763b761d6245ca72045ab5a80bcd246e0b17d465
Saumya Srivastava <saumyasrivastava498@gmail.com>
2025-08-09 21:18:20 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-
-});
+  plugins: [tailwindcss(), react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      'taskyatra.onrender.com',  
+    ]
+  }
+})
