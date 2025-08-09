0f848517951834b53cf36a298a761048642ea95b
Saumya Srivastava <saumyasrivastava498@gmail.com>
2025-08-09 20:29:40 +0530

    server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app=express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use(express.json());
